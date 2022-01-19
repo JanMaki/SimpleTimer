cd2e7b591e309c7322444373e4786b7c6de9f75d
JanMaki <mutsukimaki@gmail.com>
2022-01-19 09:43:08 +0900

    [add] 実行ボタンを実装
    押したらタイマーやダイスなどを実行することができるボタン

--- src/main/kotlin/net/necromagic/simpletimer/dice/Dice.kt
+++ src/main/kotlin/net/necromagic/simpletimer/dice/Dice.kt
@@ -1,5 +1,6 @@
 package net.necromagic.simpletimer.dice
 
+import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.interactions.Interaction
 import net.necromagic.simpletimer.ServerConfig
 import net.necromagic.simpletimer.SimpleTimer
@@ -8,8 +9,11 @@ import net.necromagic.simpletimer.util.equalsIgnoreCase
 import java.util.concurrent.Executors
 
 class Dice {
-    fun roll(event: Interaction, command: String){
+    fun roll(event: Interaction, command: String, mentionTarget: User? = null){
         var diceCommand = command
+
+        //メンションを作成
+        val mention = if (mentionTarget == null) "" else "<@${mentionTarget.idLong}>\n"
 
         //コンフィグを取得
         val config = SimpleTimer.instance.config
@@ -22,6 +26,7 @@ class Dice {
                 val isSecret = diceCommand.substring(0, 1).equalsIgnoreCase("s")
                 if (isSecret) diceCommand = diceCommand.replaceFirst("s", "")
 
+
                 //ダイスの実行
                 if (DefaultDice.checkDiceFormat(diceCommand)) {
 
@@ -31,18 +36,18 @@ class Dice {
                     //出力メッセージの作成
                     val sendMessage = if (isSecret) {
                         """
-                            (シークレットダイス)${dice.resultMessage}
+                            ${mention}(シークレットダイス)${dice.resultMessage}
                             """.trimIndent()
                     } else {
                         """
-                            ${dice.resultMessage}
+                            ${mention}${dice.resultMessage}
                             """.trimIndent()
                     }
                     //2000文字超えたときは失敗のログを出す
                     if (sendMessage.length >= 2000) {
                         event.hook.sendMessage(
                             """
-                                出力結果の文字数が多すぎます。
+                                ${mention}出力結果の文字数が多すぎます。
                                 ダイスの内容を変更して再度実行してください。
                                 """.trimIndent()
                         ).queue()
@@ -52,7 +57,7 @@ class Dice {
                     }
                 } else {
                     //構文が間違ってたらメッセージを出す
-                    event.hook.sendMessage("*ダイスの構文が間違っています").queue()
+                    event.hook.sendMessage("${mention}*ダイスの構文が間違っています").queue()
                 }
             }
 
@@ -67,10 +72,10 @@ class Dice {
                         //結果の確認
                         if (roll == null) {
                             //構文が間違ってたらメッセージを出す
-                            event.hook.sendMessage("*ダイスの構文が間違っています").queue()
+                            event.hook.sendMessage("${mention}*ダイスの構文が間違っています").queue()
                         } else {
                             //結果を出力する
-                            event.hook.sendMessage(roll).queue()
+                            event.hook.sendMessage("${mention}${roll}").queue()
                         }
                     }
                 }
---
+++ src/main/kotlin/net/necromagic/simpletimer/listener/ButtonClick.kt
@@ -0,0 +1,51 @@
+package net.necromagic.simpletimer.listener
+
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.necromagic.simpletimer.dice.Dice
+import net.necromagic.simpletimer.timer.Timer
+import java.util.*
+
+class ButtonClick:ListenerAdapter() {
+    override fun onButtonClick(event: ButtonClickEvent) {
+        super.onButtonClick(event)
+        event.deferReply().queue()
+
+        val buttonID = event.componentId
+        val channel = event.channel
+
+        //タイマーのボタン
+        if (buttonID.contains("timer")){
+            //チャンネルのタイマーを取得する
+            val channelTimers = Timer.channelsTimersMap.getOrPut(channel) { EnumMap(Timer.Number::class.java) }
+
+            //番号を確認
+            for (number in Timer.Number.values()) {
+                //その番号のタイマーが動いているかを確認
+                if (!channelTimers.containsKey(number)) {
+                    //タイマーを開始・代入
+                    channelTimers[number] =  Timer(
+                        channel,
+                        number,
+                        buttonID.replace("timer-","").toIntOrNull() ?: return
+                        , event.guild!!
+                    )
+                    Timer.channelsTimersMap[channel] = channelTimers
+                    return
+                }
+            }
+
+            //最大数のメッセージを出力する
+            event.hook.sendMessage(":x: これ以上タイマーを動かすことはできません（最大: 4）").queue()
+        }
+
+        //ダイスのボタン
+        if(buttonID.contains("dice")){
+            //ダイスのコマンドを取得
+            val value = buttonID.replace("dice-","")
+
+            //ダイスを振る
+            Dice().roll(event, value, event.user)
+        }
+    }
+}
