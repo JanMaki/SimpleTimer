e5411766b7af9431ec3316f7b6774a4efb80ab1f
JanMaki <mutsukimaki@gmail.com>
2022-03-10 04:45:12 +0900

    [fix] マージ後のエラーを修正
    マージによる影響の修正

--- src/main/kotlin/dev/simpletimer/command/DebugCommand.kt
+++ src/main/kotlin/dev/simpletimer/command/DebugCommand.kt
@@ -4,7 +4,7 @@ import dev.simpletimer.component.modal.DebugModal
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 
 object DebugCommand : SlashCommand("debug", "開発用機能", false) {
-    override fun run(command: String, event: SlashCommandInteractionEvent) {
+    override fun run(event: SlashCommandInteractionEvent) {
         event.replyModal(DebugModal.createModal(0)).queue()
     }
 }
