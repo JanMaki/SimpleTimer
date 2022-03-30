72ccf68697fe8c8b320f50c4af2ef2a87df5881b
JanMaki <mutsukimaki@gmail.com>
2022-03-30 14:54:35 +0900

    [fix] 運営用機能のログ出力が2000文字を超えることがあった
    2000文字を超えて、見ることができないことがあったので、ログを切り取りするように変更。一応念には念を入れて1500文字で切り取りしてる。

--- src/main/kotlin/dev/simpletimer/util/Log.kt
+++ src/main/kotlin/dev/simpletimer/util/Log.kt
@@ -10,16 +10,37 @@ object Log {
     //ログを送信するチャンネル
     val logChannels = mutableListOf<TextChannel>()
 
+    //ロガー
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     /**
      * ログを送信する
      */
     fun sendLog(log: String, isError: Boolean = false) {
+        //ログを切りとりする時に使用
+        var logText = log
+        //ログを切り取った結果
+        val logs = mutableListOf<String>()
+        //文字数を確認する
+        while (logText.length > 1500){
+            //最初の1500文字をリストに追加
+            logs.add(logText.substring(0,1500))
+            //最初の1500文字を削除
+            logText = logText.substring(1500, logText.length)
+        }
+        //残っている部分があるかを確認
+        if (logText.isNotEmpty() || logText.isNotBlank()){
+            //追加
+            logs.add(logText)
+        }
+
         //すべてのチャンネルを確認
         logChannels.forEach { channel ->
-            //メッセージを送信
-            channel.sendMessage(log).queue()
+            //すべてのログ
+            logs.forEach {
+                //メッセージを送信
+                channel.sendMessage(it).queue()
+            }
         }
 
         //コンソールにも出す
