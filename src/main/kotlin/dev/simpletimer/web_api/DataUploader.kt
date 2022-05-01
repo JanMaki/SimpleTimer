a08f6085762c1fb8e6c4df33f3ff0b50080840e0
JanMaki <mutsukimaki@gmail.com>
2022-05-01 09:57:58 +0900

    [change] チャンネルのデータを纏めて送るように
    Listでまとめて送るように変更

--- src/main/kotlin/dev/simpletimer/web_api/DataUploader.kt
+++ src/main/kotlin/dev/simpletimer/web_api/DataUploader.kt
@@ -7,7 +7,9 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
+import java.net.MalformedURLException
 
 /**
  * タイマーのデータのアップローダー
@@ -18,48 +20,55 @@ class DataUploader {
         //コルーチンを開始
         CoroutineScope(Dispatchers.Default).launch {
             while (true) {
-                //すべてのチャンネルを確認
-                Timer.channelsTimersMap.forEach {
-                    //タイマーのリスト
-                    val timers = arrayListOf<TimerData>()
+                try {
+                    //送信するリスト
+                    val channelDataList = mutableListOf<ChannelData>()
+                    //すべてのチャンネルを確認
+                    Timer.channelsTimersMap.forEach {
+                        //タイマーのリスト
+                        val timers = arrayListOf<TimerData>()
 
-                    //タイマーを確認
-                    it.value.apply {
-                        //0~3を確認
-                        for (number in 0..3) {
-                            //タイマーを取得
-                            val timer = this[Timer.Number.getNumber(number + 1)]
-                            //nullチェック
-                            if (timer == null) {
-                                //空データを入れる
-                                timers.add(TimerData())
-                                continue
-                            }
-                            //タイマーサービスを取得
-                            val timerService = timer.timerService
-                            val time = timerService.getTime()
-                            //タイマーのデータを追加
-                            timers.add(
-                                TimerData(
-                                    time.minute * 60 + time.seconds,
-                                    System.currentTimeMillis(),
-                                    timerService.isMove,
-                                    timerService.isFinish
+                        //タイマーを確認
+                        it.value.apply {
+                            //0~3を確認
+                            for (number in 0..3) {
+                                //タイマーを取得
+                                val timer = this[Timer.Number.getNumber(number + 1)]
+                                //nullチェック
+                                if (timer == null) {
+                                    //空データを入れる
+                                    timers.add(TimerData())
+                                    continue
+                                }
+                                //タイマーサービスを取得
+                                val timerService = timer.timerService
+                                val time = timerService.getTime()
+                                //タイマーのデータを追加
+                                timers.add(
+                                    TimerData(
+                                        time.minute * 60 + time.seconds,
+                                        System.currentTimeMillis(),
+                                        timerService.isMove,
+                                        timerService.isFinish
+                                    )
                                 )
-                            )
+                            }
                         }
+
+                        //チャンネルのデータを作成
+                        val channelData = ChannelData(it.key.idLong, timers)
+
+                        //送信するリストに追加
+                        channelDataList.add(channelData)
                     }
-
-                    //チャンネルのデータを作成
-                    val channelData = ChannelData(it.key.idLong, timers)
-
                     //Post!
                     "${SimpleTimer.instance.dataContainer.config.apiURL}/timers/${SimpleTimer.instance.dataContainer.config.apiToken}"
                         .httpPut().header(hashMapOf("Content-Type" to "application/json"))
-                        .body(Json.encodeToString(ChannelData.serializer(), channelData)).response()
+                        .body(Json.encodeToString(ListSerializer(ChannelData.serializer()), channelDataList)).response()
+                }catch (ignore: MalformedURLException) {
+                    ignore.printStackTrace()
                 }
-
-                //スレッドを0.5秒待つ
+                //スレッドを1秒待つ
                 delay(1000)
             }
         }
