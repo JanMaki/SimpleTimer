577a3b57d7d5e8164e5c059cfd542452da366f6a
JanMaki <mutsukimaki@gmail.com>
2022-11-24 20:50:20 +0900

    [fix] 空白メッセージ送信時に時間を置くように
    消せない時があったので入れて様子見

--- src/main/kotlin/dev/simpletimer/extension/InteractionHookExtension.kt
+++ src/main/kotlin/dev/simpletimer/extension/InteractionHookExtension.kt
@@ -1,5 +1,9 @@
 package dev.simpletimer.extension
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.interactions.InteractionHook
 
 //InteractionHookを拡張する
@@ -11,6 +15,11 @@ import net.dv8tion.jda.api.interactions.InteractionHook
  */
 fun InteractionHook.sendEmpty() {
     this.sendMessage("|| ||").queue {
-        it.delete().queue()
+        CoroutineScope(Dispatchers.Default).launch {
+            //1ms止める
+            delay(1)
+            //削除
+            it.delete().queue()
+        }
     }
 }
