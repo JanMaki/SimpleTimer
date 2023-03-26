7eb97b3eb0aded962ffed0843405a4fbf1eae660
JanMaki <mutsukimaki@gmail.com>
2023-03-26 10:35:57 +0900

    修正 空白メッセージの送信でエラーが出ていた

--- src/main/kotlin/dev/simpletimer/extension/InteractionHookExtension.kt
+++ src/main/kotlin/dev/simpletimer/extension/InteractionHookExtension.kt
@@ -1,9 +1,5 @@
 package dev.simpletimer.extension
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.interactions.InteractionHook
 
 //InteractionHookを拡張する
@@ -14,15 +10,5 @@ import net.dv8tion.jda.api.interactions.InteractionHook
  *
  */
 fun InteractionHook.sendEmpty() {
-    this.sendMessage("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ").queue {
-        CoroutineScope(Dispatchers.Default).launch {
-            //一時的なメッセージじゃないかを確認
-            while (!it.isEphemeral) {
-                //1ms止める
-                delay(1)
-                //メッセージを削除
-                it.delete().queue()
-            }
-        }
-    }
+    this.sendMessage("|| ||").queue()
 }
